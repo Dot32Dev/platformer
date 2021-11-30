4f603524cee070404eb015d60f128688c130a926
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-11-30 18:12:18 +0800

    now rotates to better react to jumping

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local intro = require "intro" 
 local map = require "map"
 
-local player={x=514, y=360, w=20, h=40, xV = 0, yV = 0}
+local player={x=514, y=360, w=20, h=40, xV = 0, yV = 0, r = 0}
 
 love.graphics.setBackgroundColor(intro.HSL(220/360, 0.5, 0.1))
 intro:init()
@@ -18,11 +18,13 @@ function love.update(dt)
 	end
 	player.x = player.x + player.xV
 	player.y = player.y + player.yV
+	player.r = (player.xV*1.5*math.min(1, math.max(-1, player.yV/-10)))/15
 	for i, rect in ipairs(map) do
 		if player.x+player.w > rect.x and player.x < rect.x + rect.w and player.y+player.h > rect.y and player.y < rect.y + rect.h then
 			if (player.x - player.xV)+player.w > rect.x and (player.x - player.xV) < rect.x + rect.w then 
 				player.y = (player.y < rect.y and rect.y - player.h) or (player.y > rect.y+rect.h-player.y and rect.y+rect.h) or player.y
 				player.yV = 0
+				player.r = player.xV/25
 				if love.keyboard.isDown("w") and not (player.y == rect.y+rect.h) then
 					player.yV = player.yV - 16
 				end
@@ -46,13 +48,11 @@ function love.draw()
 
 	-- player
 	love.graphics.setColour(1,1,1)
-
 	local x = player.x + player.w/2
 	local y = player.y + player.h/2
-
 	love.graphics.push()
 	love.graphics.translate(x, y)	
-	love.graphics.rotate(player.xV)
+	love.graphics.rotate(player.r)
 	love.graphics.rectangle("fill", -player.w/2, -player.h/2, player.w, player.h)
 	love.graphics.pop()
 
