f8ca4db11a1b3c5e839a318f9547b7f113dfdbf9
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-12-04 01:41:31 +0800

    intro behaves better at different screen refreshrates

--- intro.lua
+++ intro.lua
@@ -42,8 +42,8 @@ function intro:update(dt)
     error("dt is required for intro.update(dt)")
   end
 
-  self.dot32.y = self.easeOutElastic(self.timer)*love.graphics.getHeight()/2
-  self.sub.x = self.easeOutElastic(self.timer)*love.graphics.getWidth()/2
+  -- self.dot32.y = self.easeOutElastic(self.timer)*love.graphics.getHeight()/2
+  -- self.sub.x = self.easeOutElastic(self.timer)*love.graphics.getWidth()/2
 
   self.timer = self.timer + dt
   
@@ -69,9 +69,9 @@ function intro:draw()
 
     love.graphics.setColor(1, 1, 1, self.ghost)
     love.graphics.setFont(self.dot32.font)
-    love.graphics.print("Dot32", self.dot32.x - self.dot32.font:getWidth("Dot32")/2, self.dot32.y - self.dot32.font:getHeight()/2)
+    love.graphics.print("Dot32", self.dot32.x - self.dot32.font:getWidth("Dot32")/2, self.easeOutElastic(self.timer)*love.graphics.getHeight()/2 - self.dot32.font:getHeight()/2)
     love.graphics.setFont(self.sub.font)
-    love.graphics.print(self.sub.text, self.sub.x - self.sub.font:getWidth(self.sub.text)/2, self.sub.y - self.sub.font:getHeight()/2)
+    love.graphics.print(self.sub.text, self.easeOutElastic(self.timer)*love.graphics.getWidth()/2 - self.sub.font:getWidth(self.sub.text)/2, self.sub.y - self.sub.font:getHeight()/2)
 
     love.graphics.setColor(0.2, 0.2, 0.2, self.ghost)
     love.graphics.rectangle("fill", 0, love.graphics.getHeight()-5, love.graphics.getWidth()-(love.graphics.getWidth()/self.length)*self.timer, 5)
@@ -106,6 +106,10 @@ function intro.pprint(stringg, x, y)
   love.graphics.setColour(0,0,0)
   love.graphics.print(stringg, x, y)
   love.graphics.setColour(r,g,b,a)
+end
+
+function intro.distanceBetween(x1, y1, x2, y2)
+  return math.sqrt((y2-y1)^2 + (x2-x1)^2)
 end
 
 function intro.varToString(var) -- thank you so much HugoBDesigner! (https://love2d.org/forums/viewtopic.php?t=82877)
