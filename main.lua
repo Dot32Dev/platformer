c45614328c032cbe7b529d9ed733141a02a87940
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-12-04 18:24:04 +0800

    optional direction to inverse kinematics

--- main.lua
+++ main.lua
@@ -2,13 +2,23 @@ local intro = require "intro"
 local map = require "map"
 
 local player={x=514, y=360, w=10, h=40, xV = 0, yV = 0, r = 0, legWheel=0, groundTime = 0}
+local ConnectedController = false
 
 love.graphics.setBackgroundColor(intro.HSL(220/360, 0.5, 0.1))
 love.graphics.setLineWidth(5)
 love.graphics.setLineJoin("none")
 intro:init()
 
-local function inverseKinematics(p1x, p1y, l1, l2, p2x, p2y) --p1 is the foundation node, p2 is the goal node, l1/l2 are the lengths of each segment
+local function inverseKinematics(p1x, p1y, l1, l2, p2x, p2y, direction) --p1 is the foundation node, p2 is the goal node, l1/l2 are the lengths of each segment
+	if direction and direction > 0 then -- this swaps the points so as to reverse the direction in which the knee points
+		-- plx, p2x = p2x, p1x
+		-- ply, p2y = p2y, p1y
+		local p3x, p3y = p1x, p1y
+		p1x, p1y = p2x, p2y
+		p2x, p2y = p3x, p3y
+		print(math.floor(p1x), math.floor(p1y), "|", math.floor(p2x), math.floor(p2y))
+	end
+
 	local dist = intro.distanceBetween(p1x, p1y, p2x, p2y)
 
 	if dist > l1+l2 then --if the mouse is too far away then
@@ -31,30 +41,46 @@ local function lerp(a, b, t)
 	return (b-a)*t+a
 end
 
+local function resetPlayer()
+	player.x = 514
+	player.y = 360
+	player.xV = 0
+	player.yV = 0
+end 
+
 function love.update(dt)
 	-- player
 	player.yV = player.yV + 0.8 -- gravity is 0.5
 	player.xV = player.xV * 0.87--(1 / (1 + (dt * 8)))
-	if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+
+	local inputX = (ConnectedController and ConnectedController:getGamepadAxis("leftx")) or 0
+
+	if love.keyboard.isDown("a") or love.keyboard.isDown("left") or inputX < -0.2 then
 		player.xV = player.xV - 1
 	end
-	if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+	if love.keyboard.isDown("d") or love.keyboard.isDown("right") or inputX > 0.2 then
 		player.xV = player.xV + 1
 	end
+
 	player.x = player.x + player.xV
 	player.y = player.y + player.yV
+
 	local colided = false
 	for i, rect in ipairs(map) do
 		if player.x+player.w > rect.x and player.x < rect.x + rect.w and player.y+player.h > rect.y and player.y < rect.y + rect.h then
 			if (player.x - player.xV)+player.w > rect.x and (player.x - player.xV) < rect.x + rect.w then 
 				player.y = (player.y < rect.y and rect.y - player.h) or (player.y > rect.y+rect.h-player.y and rect.y+rect.h) or player.y
 				player.yV = 0
+
 				player.r = player.xV/25
 				player.groundTime = player.groundTime + 1
 				player.legWheel = player.legWheel + 15/180*math.pi*sign(player.xV)--player.xV/80*math.pi
-				if (love.keyboard.isDown("w") or love.keyboard.isDown("up")) and not (player.y == rect.y+rect.h) then
+
+				if (love.keyboard.isDown("w") or love.keyboard.isDown("up") or (ConnectedController and ConnectedController:isGamepadDown("a", "y"))) 
+				and not (player.y == rect.y+rect.h) then
 					player.yV = player.yV - 16
 				end
+
 				colided = true
 			else
 				player.x = (player.x < rect.x and rect.x - player.w) or (player.x > rect.x+rect.w-player.x and rect.x+rect.w) or player.x
@@ -62,11 +88,15 @@ function love.update(dt)
 			end
 		end
 	end
+
 	if not colided then
 		player.legWheel = player.legWheel % math.pi
 		player.legWheel = player.legWheel * 0.9
 		player.r = (player.xV*0.8*math.min(1, math.max(-1, player.yV/-7)))/15
 		player.groundTime = 0
+	end
+	if player.y > 720 or (ConnectedController and ConnectedController:isGamepadDown("dpleft", "dpright", "dpup", "dpdown")) then 
+		resetPlayer()
 	end
 
 	-- intro
@@ -100,17 +130,13 @@ function love.draw()
 	love.graphics.line(x,y, x+math.sin(player.r+math.pi+dir*math.sin(player.legWheel)*player.xV/5)*14,y+math.cos(player.r+dir*math.sin(player.legWheel)*player.xV/5)*14)	
 	-- love.graphics.line(x,y, footX, footY)
 
-	if player.xV > 0 then 
-		love.graphics.line(inverseKinematics(x,y,7,7,footX,footY))
-	else 
-		love.graphics.line(inverseKinematics(footX,footY,7,7,x,y))
-	end
+	love.graphics.line(inverseKinematics(footX,footY,7,7,x,y, player.xV))
 
 	-- intro
 	intro:draw()
 
 	-- test
-	love.graphics.setColour(1,0,0)
+	-- love.graphics.setColour(1,0,0)
 	-- love.graphics.line(inverseKinematics(400, 300, 100, 100, love.mouse.getX(), love.mouse.getY()))
 	-- if math.cos(player.legWheel)*player.xV/5 < 0 then love.graphics.setColour(0,0, 1) end
 	-- love.graphics.circle("fill", 200+player.r+math.pi+math.sin(player.legWheel)*player.xV/5*100, 100, 10)
@@ -119,13 +145,22 @@ function love.draw()
 	 -- x+math.sin(player.r+math.pi-1*player.xV/5)*14,y+math.cos(player.r-1*player.xV/5)*14)
 
 	 -- love.graphics.circle("fill", lerp(x+math.sin(player.r+math.pi+dir*player.xV/5)*14, x+math.sin(player.r+math.pi-dir*player.xV/5)*14, (math.sin(player.legWheel)+1)*0.5), lerp(y+math.cos(player.r+dir*player.xV/5)*14, y+math.cos(player.r-dir*player.xV/5)*14, (math.sin(player.legWheel)+1)*0.5) , 3)
+	 if ConnectedController then love.graphics.print(ConnectedController:getName()) end
 end
 
 function love.keypressed(k)
 	if k == "r" then
-		player.x = 514
-		player.y = 360
-		player.xV = 0
-		player.yV = 0
+		resetPlayer()
 	end	
+end
+
+function love.joystickadded(j)
+	if not ConnectedController then
+		ConnectedController = j
+	end
+end
+function love.joystickremoved(j)
+	if j == ConnectedController then
+		ConnectedController = false
+	end
 end
